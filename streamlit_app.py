d8f068fb4720ecd414414b29d82c8692a610ccd6
Waldemar Herter <herter.waldemar@gmail.com>
2025-07-15 18:32:38 +0000

    asdf

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 # app.py
-=======
->>>>>>> 4a2501e (asd)
+
 import streamlit as st
 import pandas as pd
 import plotly.express as px
@@ -116,8 +113,3 @@ if st.button("Daten aktualisieren", key="refresh"):
 # .streamlit/secrets.toml Beispiel
 # [default]
 # MONGO_URI = "mongodb+srv://user:password@whcluster0.kxjuhm0.mongodb.net/weight_tracker?..."
-<<<<<<< HEAD
-=======
->>>>>>> 6e4dcff (edit)
-=======
->>>>>>> 4a2501e (asd)
